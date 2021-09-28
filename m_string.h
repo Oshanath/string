23ca5ebd3b5b5f5db71c44765261bd2e0e4f2bf4
Oshanath Rajawasam <oshanath.19@cse.mrt.ac.lk>
2021-09-29 01:05:51 +0530

    added occurence counting to find_all

--- m_string.h
+++ m_string.h
@@ -11,16 +11,25 @@ namespace string{
  * 
  * @param haystack The string that needs to be searched
  * @param needle The substring to look for inside the haystack
+ * @param occurences maximum number of search results needed
  * @return std::vector<size_t> The container of indices of the needle substring inside the haystack
  */
-std::vector<size_t> find_all(const std::string& haystack, const std::string& needle){
+std::vector<size_t> find_all(const std::string& haystack, const std::string& needle, size_t occurences = -1){
+
+    /**
+     * When occurences = -1, since it is a size_t that'll be the largest number that type can hold, according to the rules of
+     * 2's complement. In the function we count the found_needles and if the quota is reached (found_needles == occurences)
+     * the loop will return the results.
+     */
 
     std::vector<size_t> list;
 
     size_t i = haystack.find(needle);
+    size_t found_needles = 0;
 
-    while(i != std::string::npos){
+    while(i != std::string::npos && found_needles < occurences){
         list.push_back(i);
+        found_needles++;
         i = haystack.find(needle, i + needle.length());
     }
 
@@ -32,14 +41,15 @@ std::vector<size_t> find_all(const std::string& haystack, const std::string& nee
  * 
  * @param haystack The string that needs to be searched
  * @param needle The character to look for inside the haystack
+ * @param occurences maximum number of search results needed
  * @return std::vector<size_t> The container of indices of the needle substring inside the haystack
  */
 
-std::vector<size_t> find_all(const std::string& haystack, const char c){
+std::vector<size_t> find_all(const std::string& haystack, const char c, size_t occurences = -1){
     std::string char_string;
     char_string.push_back(c);
 
-    return find_all(haystack, std::move(char_string));
+    return find_all(haystack, std::move(char_string), occurences);
 }
 
 
